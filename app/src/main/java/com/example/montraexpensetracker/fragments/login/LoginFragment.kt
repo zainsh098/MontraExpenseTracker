6840161958322aee59a578934e1d98a4091924ec
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-08-12 18:10:13 +0500

    Issues Solved Related Login

--- app/src/main/java/com/example/montraexpensetracker/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/example/montraexpensetracker/fragments/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.montraexpensetracker.R
 import com.example.montraexpensetracker.data.remote.FirebaseAuthDataSource
@@ -13,16 +14,15 @@ import com.example.montraexpensetracker.data.respository.AuthRepositoryImpl
 import com.example.montraexpensetracker.databinding.FragmentLoginBinding
 import com.example.montraexpensetracker.presentation.viewmodel.LoginViewModel
 
-
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
     private val authRepository = AuthRepositoryImpl(FirebaseAuthDataSource())
-
     private val loginViewModel = LoginViewModel(authRepository)
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
         return binding.root
@@ -31,8 +31,29 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        loginViewModel.userLoginLiveData.observe(viewLifecycleOwner, Observer { userLoginModel ->
+            if (userLoginModel != null) {
+                findNavController().navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
+            } else {
+                Toast.makeText(context, "User not signed in", Toast.LENGTH_SHORT).show()
+            }
+        })
+
         binding.apply {
             componentToolbar.textToolbar.text = getString(R.string.text_toolbar_login)
+
+componentToolbar.backArrow.setOnClickListener{
+
+    findNavController().navigate(R.id.action_loginFragment_to_viewPagerFragment)
+
+}
+
+textViewSignUp.setOnClickListener {
+    findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+
+}
+
+
             buttonLogin.setOnClickListener {
                 val email = textFieldEmail.text.toString().trim()
                 val password = textFieldPassword.text.toString().trim()
@@ -40,24 +61,12 @@ class LoginFragment : Fragment() {
                 if (email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(context, "Please fill all fields", Toast.LENGTH_SHORT).show()
                 } else {
-                    loginViewModel.userLoginLiveData.observe(viewLifecycleOwner) { userLoginModel ->
-                        if (userLoginModel != null) {
-
-                            loginViewModel.signIn(email, password)
-                            findNavController().navigate(
-                                R.id.action_loginFragment_to_forgotPasswordFragment,
-                            )
-                        } else {
-                            Toast.makeText(context, "User not signed in", Toast.LENGTH_SHORT).show()
-                        }
-                    }
+                    loginViewModel.signIn(email, password)
                 }
             }
 
             textViewForgotPassword.setOnClickListener {
-
                 findNavController().navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
-
             }
         }
     }
--- app/src/main/java/com/example/montraexpensetracker/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/montraexpensetracker/presentation/viewmodel/LoginViewModel.kt
@@ -8,26 +8,18 @@ import com.example.montraexpensetracker.domain.AuthRepository
 import com.example.montraexpensetracker.domain.UserLoginModel
 import kotlinx.coroutines.launch
 
-class LoginViewModel(private val authRepository: AuthRepository,) : ViewModel() {
-
+class LoginViewModel(private val authRepository: AuthRepository) : ViewModel() {
 
     val userLoginLiveData = MutableLiveData<UserLoginModel?>()
 
     fun signIn(email: String, password: String) {
-
         viewModelScope.launch {
-
-
             try {
                 val user = authRepository.loginUser(email, password)
                 userLoginLiveData.value = user
             } catch (e: Exception) {
                 Log.e("AuthViewModel", "Error during sign up", e)
             }
-
-
         }
-
     }
-
 }
